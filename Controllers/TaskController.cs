6815bd5c24a4ade86612662a1020fa2a883804ce
“Lucas <lucasrech00@gmail.com>
2024-01-08 00:45:50 -0300

    Create Index controller and razor page

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TasksList.Data;
+using TasksList.Models;
+
+namespace TasksList.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TaskController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var task = _context.Tasks.ToList();
+            return View(task);
+        }
+
+
+    }
+}
